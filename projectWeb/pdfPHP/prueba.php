23eeab2114e434a62fa130c20b37e57fd4f8224b
Adrian González Pardo <gozapaadr@gmail.com>
2019-06-02 11:15:17 -0500

    Fix the pdf path

--- projectWeb/pdfPHP/prueba.php
+++ projectWeb/pdfPHP/prueba.php
@@ -4,7 +4,7 @@ include 'index.php';
 /*Cambia la ruta de var y todo eso para que jale en winbugs*/
   try{
     $client = new \Pdfcrowd\HtmlToPdfClient("demo", "ce544b6ea52a5621fb9d55f8b542d14d");
-    $client->convertFileToFile("/var/www/html/projectWeb/pdfPHP/pdfFile.html", "pdfCasei.pdf");
+    $client->convertFileToFile(__DIR__."/pdfFile.html", "pdfCasei.pdf");
     header("location:/projectWeb/pdfPHP/pdfCasei.pdf");
   }catch(\Pdfcrowd\Error $why){
     error_log("Pdfcrowd Error: {$why}\n");
